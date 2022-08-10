196e7e7ae9bd9cb88a8d8b7ecdd1c7e8919a4379
Ibiye Charles <c.ibiye@gmail.com>
2022-08-10 08:19:30 -0700

    fix

--- _printf.c
+++ _printf.c
@@ -14,7 +14,7 @@ int (*check_format(const char *format))(va_list)
 	print_t p[] = {
 		{"c", print_c},
 		{"s", print_s},
-		{"i", print_i},
+/**		{"i", print_i},
 		{"d", print_d},
 		{"b", print_b},
 		{"u", print_u},
@@ -25,7 +25,7 @@ int (*check_format(const char *format))(va_list)
 		{"S", print_S},
 		{"r", print_r},
 		{"R", print_R},
-		{NULL, NULL}
+		{NULL, NULL}*/
 	};
 	for (; p[i].t != NULL; i++)
 	{
